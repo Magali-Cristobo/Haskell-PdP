1b7ac971b21dffd9cf8c89dbd9d4b9504c8cc9ff
Magali-Cristobo <alumno21.cristobo.magali@ipm.edu.ar>
2025-05-07 16:57:26 -0300

    funcion all y map

--- practica3.hs
+++ practica3.hs
@@ -8,10 +8,28 @@ filterOS:: (a -> Bool) -> [a] -> [a]
 filterOS f [] = []
 filterOS f (x:xs)  = if(f x) then (x:(filterOS f xs )) else (filterOS f xs )
 
+-- otra forma 
+{-
+filter c (x:xs)
+    | c x = x: filter c xs
+    | otherwise = filter condicion xs
+
+-}
 sacaParesOS:: [Int] -> [Int]
 -- sacaParesOS = filterOS (\x -> odd x)
 sacaParesOS = filterOS odd 
 
+-- map::(a -> b) -> [a] -> [b]
+-- map f [] = []
+-- map f (x:xs) = f x : map f cola
+
+-- all si todos los elementos de la lista cumplen un criterio 
+-- all :: (a -> Bool) -> [a] - Bool
+-- all _ [] = []
+-- all condicion (x:xs) = condicion x && all condicion xs
+
+hacerNVeces 0 f valor = valor
+hacerNVeces n f valor | n > 0 = hacerNVeces (n-1) f (f valor)
 
 -- 1 Definir una función esCerrada, que dada una lista y una función binaria (que toma un par de elementos del tipo de la lista y devuelve un elemento 
 -- del tipo de la lista) devuelve si la función es cerrada respecto a la lista de entrada (o sea, si la imagen de la función 
@@ -19,3 +37,5 @@ sacaParesOS = filterOS odd
 
 esCerrada:: Eq a => [a] -> (a -> a  -> a) -> Bool
 esCerrada xs f = and [ f x y `elem` xs | x <- xs, y <- xs]
+
+maxf:: Eq a => (a -> [a]) -> a
